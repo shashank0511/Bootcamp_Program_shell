3099d2a13220f8e65253e7049cf416785e44961b
shashank0511 <61483511+shashank0511@users.noreply.github.com>
2020-04-27 16:25:31 +0530

    flipcoins

---
+++ flipcoins/SingleFlip.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "flip Coin Simulator"
+
+echo "number of times user want to flip the coin"
+read flip
+echo $flip
+
+countHead=0
+countTail=0
+declare -A noOfCounts
+for((i=0;i<flip;i++))
+do
+if [ $((RANDOM%2)) -eq 1 ]
+then
+((countHead++))
+else
+((countTail++))
+fi
+done
+echo "head="$countHead
+echo "tail="$countTail
+
+noOfCounts[tail]=$countTail
+noOfCounts[head]=$countHead
+
+echo ${noOfCounts[@]}
+
+PercentageOfTails=$((100*$countTail/$flip))
+echo "Percentage of tails= "$PercentageOfTails
+PercentageOfHeads=$((100*$countHead/$flip))
+echo "Percentage of heads= "$PercentageOfHeads
