cbf6ca8a46e2be8d0105c0f30195ae495ace334c
shashank0511 <61483511+shashank0511@users.noreply.github.com>
2020-04-27 16:23:58 +0530

    added

---
+++ arthematic_operation/arth-_asc_desc.sh
@@ -0,0 +1,79 @@
+#! /bin/bash 
+
+read -p "Enter a:" a
+read -p "Enter b:" b
+read -p "Enter c:" c
+
+declare -A expressionResultDict
+declare -a expressionResultArray
+
+
+function computeExpressions() {
+    expression=$1
+    a=$2
+    b=$3
+    c=$4
+    case $expression in 
+        1)
+        result=$(($a + $b * $c ))
+        ;;
+        2)
+        result=$(($a * $b + $c ))
+        ;;
+        3)
+        result=$(($c + $a / $b ))
+        ;;
+        4)
+        result=$(($a % $b + $c ))
+        ;;
+    esac
+    
+    echo $result
+
+}
+
+function expressionPerform() {
+    expression=$1
+   
+    case $expression in 
+        1)
+        exp="a+b*c"
+        ;;
+        2)
+        exp="a*b+c"
+        ;;
+        3)
+        exp="c+a/b"
+        ;;
+        4)
+        exp="a%b+c"
+        ;;
+    esac
+    
+    echo $exp
+}
+for (( counter=1;counter<=4;counter++ ))
+do
+    expressionResult=$( computeExpressions $counter $a $b $c )
+    expressionForm=$( expressionPerform $counter )
+    expressionResultDict[$expressionForm]=$expressionResult
+done
+
+
+echo "The computation expressions result are"
+
+counter=0
+for i in ${!expressionResultDict[@]}
+do
+    echo $i"="${expressionResultDict[$i]}
+    expressionResultArray[((counter++))]=${expressionResultDict[$i]}
+done
+
+
+echo "The computation expressions result in Ascending"
+sortedAsc=($(echo ${expressionResultArray[*]}| tr " " "\n" | sort -n))
+echo ${sortedAsc[@]}
+
+echo "The computation expressions result in Descending"
+sortedDesc=($(echo ${expressionResultArray[*]}| tr " " "\n" | sort -nr))
+echo ${sortedDesc[@]}
